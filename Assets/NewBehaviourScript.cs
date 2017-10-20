b70786502e19ef7de6fafbf4c786e11539f6fbab
Akimakisu <davidmesco@hotmail.com>
2017-10-19 19:53:57 -0500

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float time;
+    int i;
+
+    // Use this for initialization
+    public Patrullar(float viewDistance, float wait, GameObject waypoints) : base(viewDistance, wait, waypoints)
+        { }
+
+    
+    void Start () {
+        time = wait;
+	}
+	
+	// Update is called once per frame
+	public override ExcecuteProfile() {
+
+        Moverse();
+	}
+    public void Moverse() {
+
+        if (time >= 1)
+        {
+            if (i < route.transform.childCount) 
+            {
+                this.transform.position = new Vector3(route.transform.GetChild(i).transform.position.x, route.transform.GetChild(i).transform.position.y, route.transform.GetChild(i).transform.position.z);
+                        
+                if (i == route.transform.childCount - 1) 
+                {
+                    i = 0; 
+                }
+                else 
+                {
+                    i++;
+                }
+
+            }
+            time = 0; 
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/Waypoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+    private Transform[] waypoints;
+	// Use this for initialization
+	void Start () {
+        waypoints = new transform[transform.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = transform.GetChild(i);
+        }
+	}
+
+    public Transform[] Waypointsitos
+    {
+        get
+        {
+            return waypoints;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
